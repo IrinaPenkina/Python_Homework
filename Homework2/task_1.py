23e6d9214926aa2e38ac5dcf203d0d84ad3305ec
IrinaPenkina <nikolay_irina@mail.ru>
2023-02-24 19:45:52 +0300

    homework2 checked

--- Homework2/task_1.py
+++ Homework2/task_1.py
@@ -4,23 +4,41 @@
 # чтобы все монетки были повернуты вверх одной и той же стороной. 
 # Выведите минимальное количество монет, которые нужно перевернуть.
 
-from random import *
-n = int(input('Задайте количество монет: '))
-coins = []
-count1 = 0
-count2 = 0
+# from random import *
+# n = int(input('Задайте количество монет: '))
+# coins = []
+# count1 = 0
+# count2 = 0
 
-for i in range(n):
-    c = randint(0, 1)
-    if c == 0:
-        count1 += 1
-    else:
-        count2 += 1
-    coins.append(c)
+# for i in range(n):
+#     c = randint(0, 1)
+#     if c == 0:
+#         count1 += 1
+#     else:
+#         count2 += 1
+#     coins.append(c)
 
+# print(coins)
+
+# if count1 < count2:
+#     print(count1)
+# else:
+#     print(count2)
+
+# import random
+
+# n = int(input('Задайте количество монет: '))
+# a = [random.randint(0,1) for _ in range (n)]
+# print(a)
+# if a.count(0) <= a.count(1):
+#     print(a.count(0))
+# else:
+#     print(a.count(1))
+
+# ВАЖНО: этот алгоритм работает дольше из-за применения метода count.
+# Сложность вметода count возрастает нелинейно при увеличении массива.
+
+from random import randint
+coins = [randint(0, 1) for _ in range(int(input('Введите количество монет: ')))]
 print(coins)
-
-if count1 < count2:
-    print(count1)
-else:
-    print(count2)
+print(min(coins.count(0), coins.count(1)))
--- Homework2/task_2.py
+++ Homework2/task_2.py
@@ -7,24 +7,33 @@
 # Помогите Кате отгадать задуманные Петей числа.
 
 
-import math
+# import math
 
-s = int(input('Задайте сумму чисел: '))
-n = int(input('Задайте произведение чисел: '))
+# s = int(input('Задайте сумму чисел: '))
+# n = int(input('Задайте произведение чисел: '))
 
-dis = s * s - 4 * n 
-sqrt_val = math.sqrt(abs(dis)) 
+# dis = s * s - 4 * n 
+# sqrt_val = math.sqrt(abs(dis)) 
 
-if dis > 0:
-    x = round((-s + sqrt_val) /(-2))
-    y = round(s - x, 0)
-    print(f"Значения: {x}, {y}") 
+# if dis > 0:
+#     x = round((-s + sqrt_val) /(-2))
+#     y = round(s - x, 0)
+#     print(f"Значения: {x}, {y}") 
     
-elif dis == 0:
-    x = round(s / 2, 0)
-    y = round(s - x, 0)
-    print(f"Значения: {x}, {y}")
+# elif dis == 0:
+#     x = round(s / 2, 0)
+#     y = round(s - x, 0)
+#     print(f"Значения: {x}, {y}")
  
-else: 
-    print('Значений нет')
+# else: 
+#     print('Значений нет')
  
+
+x = int(input('Задайте сумму чисел: '))
+y = int(input('Задайте произведение чисел: '))
+for i in range(x):
+    for j in range(y):
+        if x == i + j and y == i * j:
+            print(i,j)
+
+# ВАЖНО: Решение через дискриминант самое простое и оптимальное.
