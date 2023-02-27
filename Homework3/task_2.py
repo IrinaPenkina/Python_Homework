6590e9cb1ed538d73b5b2ce51236a76ae3dd5c01
IrinaPenkina <nikolay_irina@mail.ru>
2023-02-27 20:48:06 +0300

    homework discussed

--- Homework3/task_2.py
+++ Homework3/task_2.py
@@ -15,15 +15,23 @@ from random import randint
 lst = [randint(-10, 10) for _ in range(int(input('Задайте количество элементов массива: ')))]
 print(f"Массив: {lst}")
 
-k = int(input('Задайте целое число: '))
+# k = int(input('Задайте целое число: '))
 
-difference =  abs(lst[0] - k)
-target = lst[0]
+# difference =  abs(lst[0] - k)
+# target = lst[0]
 
-for i in range(1, len(lst)):
-    diff = abs(lst[i] - k)
-    if diff < difference:
-        difference = diff
-        target = lst[i]
+# for i in range(1, len(lst)):
+#     diff = abs(lst[i] - k)
+#     if diff < difference:
+#         difference = diff
+#         target = lst[i]
 
-print(f"Самый близкий элемент массива к числу {k} -> {target}.")
+# print(f"Самый близкий элемент массива к числу {k} -> {target}.")
+
+
+x = int(input('x = '))
+
+dct = {abs(x - item): item for item in lst}
+
+print(dct)
+print(dct[min(dct)])
